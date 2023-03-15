f03696d2db395f2c97467fd7d6d2d3c55a0cd705
the-datageek <moraaf770@gmail.com>
2023-03-15 15:27:06 -0400

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "seeding started"
+Student.create!(first_name: 'Faith', last_name: 'Moraa', grade: 'A')
+Student.create!(first_name: 'Naomi', last_name: 'Campbell', grade: 'B')
+Student.create!(first_name: 'Tinah', last_name: 'Mojo', grade: 'C')
+Student.create!(first_name: 'Brick', last_name: 'Lace', grade: 'D')
+puts "done seeding"
